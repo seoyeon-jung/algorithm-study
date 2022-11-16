1cabc0f975e1e0f952fe041a85cc88ad2d21c8c1
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2022-11-16 17:21:49 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,18 @@
+function solution(num_list) {
+    var answer = [];
+    let odd = 0;
+    let even = 0;
+    
+    for (let i = 0; i < num_list.length; i++) {
+        if (num_list[i] % 2 === 0) {
+            even++;
+        }
+        else {
+            odd++;
+        }
+    }
+    answer.push(even)
+    answer.push(odd)
+    
+    return answer;
+}
