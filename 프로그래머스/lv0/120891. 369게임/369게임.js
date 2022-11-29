1f285a514167d4014e5fae46a982d49d341ee77e
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2022-11-29 15:47:36 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,4 +1,12 @@
 function solution(order) {
-    var answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
+    var answer = 0;
+    const num = order.toString()
+    
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] ==3 || num[i] == 6 || num[i] == 9) {
+            answer += 1;
+        }
+    }
+    
     return answer;
 }
