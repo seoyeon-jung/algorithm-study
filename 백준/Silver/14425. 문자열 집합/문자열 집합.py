f3ad2bc360acf1c233e7a5660195837c1119bd3a
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2024-03-28 20:02:55 +0900

    [Silver IV] Title: 문자열 집합, Time: 116 ms, Memory: 36796 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,10 +1,13 @@
-N, M = map(int, input().split())
-S = set([input() for _ in range(N)])
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+S = set([sys.stdin.readline().strip() for _ in range(N)])
 count = 0
 
+# 나열된 문자엳들 돌면서 S에 포함되어있는지 확인
 for _ in range(M):
-    check = input()
-    if check in S:
+    check_word = sys.stdin.readline().strip()
+    if check_word in S:
         count += 1
 
 print(count)
