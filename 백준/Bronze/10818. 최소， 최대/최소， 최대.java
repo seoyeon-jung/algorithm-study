af23f1ea828e03d5aaed63c028064bb0c47a2b4b
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2024-07-12 16:33:11 +0900

    [Bronze III] Title: 최소, 최대, Time: 2224 ms, Memory: 321708 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] numArr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			numArr[i] = sc.nextInt();
+		}
+
+		sc.close();
+		Arrays.sort(numArr);
+		System.out.print(numArr[0] + " " + numArr[N - 1]);
+	}
+
+}
