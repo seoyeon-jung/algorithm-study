80902e8c7df481975908ec0e2975999cd29363a9
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2024-08-10 10:22:25 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+N, M = map(int, input().split())
+graph = []
+
+for _ in range(N):
+    graph.append(list(map(int, input())))
+
+def bfs(x, y):
+    # 이동할 상하좌우 방향 정의
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 방향 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 위치에 벗어나지 않아야 한다
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                continue
+            # 벽이면 진행 X
+            if graph[nx][ny] == 0:
+                continue
+            # 벽이 아니어야 이동 가능
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    # 마지막 값에서 카운트 값 가져오기
+    return graph[N - 1][M - 1]
+
+print(bfs(0, 0))
