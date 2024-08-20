c89c33ee1e269529631873580b1a8647cfd8a21f
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2024-08-20 14:47:54 +0900

    [Silver IV] Title: 제로, Time: 2424 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,10 +1,16 @@
-n = int(input())
-result = []
+# 정수 k
+k = int(input())
 
-for i in range(n):
-    num = int(input())
+# k줄에 정수 1개씩 주어짐
+numArr = [] # 최종적으로 더할 숫자들의 list
+
+for i in range(k):
+    num = int(input()) # k줄만큼 숫자 입력해야 함
+    # 잘못된 수를 부를 때마다 0 (가장 최근 수 삭제)
     if num == 0:
-        result.pop()
+        numArr.pop()
     else:
-        result.append(num)
-print(sum(result))
+        numArr.append(num)
+
+# 모든 수의 합을 출력
+print(sum(numArr))
