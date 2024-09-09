e2ce8fba8bfa4eff2f1cf292c5d6855ad8c33ce4
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2024-09-10 08:51:27 +0900

    [Bronze II] Title: 수 정렬하기, Time: 52 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,9 +1,10 @@
-N = int(input())
+N = int(input()) # 수의 개수
 num = []
 
 for i in range(N):
-    num.append(int(input()))
-answer = sorted(num)
+    num.append(int(input())) # N개만큼 숫자 입력
+
+answer = sorted(num) # 오름차순으로 정렬
 
 for i in range(len(num)):
     print(answer[i])
