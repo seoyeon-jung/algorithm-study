3641f5ae55981daec63a4d1acc04bcc9287edcdd
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2024-11-17 19:34:41 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.26 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,17 +1,17 @@
 class Solution {
     public StringBuilder solution(String my_string, int s, int e) {
-        String[] newStr = my_string.split("");
         StringBuilder answer = new StringBuilder();
         
-        for (int i = 0; i < newStr.length; i++) {
-            if (i == s) {
-                for (int j = e; j >= s; j--) {
-                    answer.append(newStr[j]);
-                }
-            }
-            if (i < s || i > e) {
-                answer.append(newStr[i]);
-            }
+        for (int i = 0; i < s; i++) {
+            answer.append(my_string.charAt(i));
+        }
+        
+        for (int i = e; i >= s; i--) {
+            answer.append(my_string.charAt(i));
+        }
+        
+        for (int i = e + 1; i < my_string.length(); i++) {
+            answer.append(my_string.charAt(i));
         }
         
         return answer;
