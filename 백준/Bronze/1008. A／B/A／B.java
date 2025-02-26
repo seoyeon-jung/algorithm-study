98874103c7e9ea4d4516febd2b11fed9a1475bad
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2025-02-26 18:48:20 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17960 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,16 +1,11 @@
 import java.util.Scanner;
- 
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in=new Scanner(System.in);
-		
-		double a=in.nextDouble();
-		double b=in.nextDouble();
-		
-		in.close();
-		System.out.print(a/b);
- 
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        System.out.println(A/B);
+    }
 }
