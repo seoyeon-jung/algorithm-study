0f5e8138c6518c8f53d1f5a66d53bf93afe378bb
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2025-02-28 18:15:56 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 132 ms, Memory: 15008 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // N명의 사람들, 양의 정수 K
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        // 순서대로 K번째 사람 제거 (N명이 제거될때까지)
+        // 1. list 통해서 k번째 사람 제거
+        // 2. 제거 위치 계산 (현재 위치 + K)해서 k번째 사람 제거
+        // 제거를 반복해야한다
+
+        int index = 0; // 현재 위치 변수
+        ArrayList<Integer> list = new ArrayList<>(); // list 형식으로 N명 리스트화
+        for(int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+        // print 형식: < >
+        bw.write("<");
+
+        while(list.size() > 1) {
+            // k 더했을때 현재 인원수보다 커지는 경우 처음 인원으로 돌아간다
+            index = index + (K-1) < list.size() ? index + (K-1) : (index+(K-1)) % list.size();
+            bw.write(list.get(index) + ", "); // 제거 위치 저장
+            list.remove(index);
+        }
+
+        bw.write(list.get(0) + ">"); // 마지막 남은 사람 + > 추력
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
