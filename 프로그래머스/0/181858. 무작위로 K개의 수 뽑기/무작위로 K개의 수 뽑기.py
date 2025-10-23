21e9c636eebef23ef7520145a97f400b002b4b53
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2025-10-23 20:37:19 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 62.71 ms, Memory: 9.72 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,9 @@
+def solution(arr, k):
+    result = []
+    for i in arr:
+        if i not in result:
+            result.append(i)
+        if len(result) == k:
+            break
+    
+    return result + [-1] * (k - len(result))
